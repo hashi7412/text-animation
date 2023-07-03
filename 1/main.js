27cb0d2fbb94b2a6fac84ab2ad35c32cd745ca09
Popcorn412 <felix44291@gmail.com>
2023-07-03 04:25:06 -0500

    Update functionality

--- 1/main.js
+++ 1/main.js
@@ -1,21 +1,37 @@
+const animeFlag = 1;
+
+const animeTypes = [
+	
+]
+
 const createSpan = (text, index) => {
-    const node = document.createElement("span");
+  const node = document.createElement("span");
 
-    node.textContent = text;
-    node.style.setProperty('--index', index);
+  node.textContent = text;
+  node.style.setProperty("--index", index);
 
-    return node;
-}
+  node.addEventListener("mouseover", () => {
+	node.classList.add("hovered");
+  })
 
-const splitWord = (text) => {
-    return text.split(' ').map(createSpan);
-}
+  return node;
+};
+
+const splitWays = [
+  (text) => {
+    return text.split(" ").map(createSpan);
+  },
+  (text) => {
+    const arr = text.split("");
+    return arr.map((i) => createSpan(i === " " ? " " : i))
+  }
+];
 
 (() => {
-    const textTargets = document.querySelectorAll("[animated-text]");
+  const textTargets = document.querySelectorAll("[animated-text]");
 
-    textTargets.forEach(node => {
-        const childNodes = splitWord(node.innerText);
-        node.replaceChildren(...childNodes);
-    })
-})()
+  textTargets.forEach((node) => {
+    const childNodes = splitWays[animeFlag](node.innerText);
+    node.replaceChildren(...childNodes);
+  });
+})();
